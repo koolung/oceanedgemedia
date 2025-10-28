35540139060a51728eab90261aedec347e634efc
BWS Developer <koolung43@gmail.com>
2025-10-28 10:43:14 -0300

    &apos;

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.2 }}
             >
-              The Last Agency You'll <br></br><span className="gradient-text">Ever Need</span>
+              The Last Agency You&apos;ll <br></br><span className="gradient-text">Ever Need</span>
             </motion.h1>
 
             <motion.p
