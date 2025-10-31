fe0cfa9ba9bf27e225e60a1f0ca4a99ab733870f
BWS Developer <koolung43@gmail.com>
2025-10-31 14:45:57 -0300

    minor image issues

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -13,19 +13,19 @@ const testimonials: Testimonial[] = [
     name: 'Magical Memories Rentals',
     role: 'Business Owner',
     content: 'Working with Ocean Edge Media has been an absolute game-changer for our business! Her expertise, dedication and attention to detail have been invaluable. She goes above and beyond to ensure our content is engaging, on-trend, and aligned with our goals. More importantly, she is an absolute pleasure to work with - professional, responsible, and truly invested in our success! We highly recommend Ocean and Maritime Social Solutions to anyone looking to elevate their digital presence. She is a true asset, and we are beyond grateful for all her hard work!',
-    logo: '/images/portfolio/mmr.jpg',
+    logo: '/images/partners/mmr.jpg',
   },
   {
     name: 'Rachelle Uveges',
     role: 'Business Owner',
     content: 'Ocean Edge Media did a wonderful job of creating content that is specific to my business and connects with my target audience. They took the time to understand my brand, message and goals and quickly delivered quality content that makes a real impact (and makes my job much easier!). Quick to respond, attentive to detail and lovely to work with!',
-    logo: '/images/portfolio/ru.jpg',
+    logo: '/images/partners/ru.jpg',
   },
   {
     name: 'Henman Properties',
     role: 'Property Management',
     content: 'Working with Ocean on our property management website was a game-changer. She\'s incredibly knowledgeable in website design, fast, efficient, and-most importantly-patient with all my indecisiveness. She made the process smooth and stress-free. On top of that, her services are very affordable. Highly recommend her for anyone looking to build a professional site without the headache!',
-    logo: '/images/portfolio/henman.jpg',
+    logo: '/images/partners/henman.jpg',
   },
 ];
 
